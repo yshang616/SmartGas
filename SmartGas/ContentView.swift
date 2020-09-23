307edb97718f2fabcbfc325f27802da2410bae37
yshang616 <yshang@macalester.edu>
2020-09-23 18:57:40 +0800

    Initial Commit

---
+++ SmartGas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartGas
+//
+//  Created by 商语童 on 2020/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmartGas/SmartGasApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartGasApp.swift
+//  SmartGas
+//
+//  Created by 商语童 on 2020/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SmartGasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
