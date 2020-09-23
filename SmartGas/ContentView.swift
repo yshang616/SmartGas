36ca51aed43cb4ae370f77d11d784f5307f23915
yshang616 <yshang@macalester.edu>
2020-09-23 21:27:54 +0800

    Launch Page

--- SmartGas/ContentView.swift
+++ SmartGas/ContentView.swift
@@ -2,20 +2,31 @@
 //  ContentView.swift
 //  SmartGas
 //
-//  Created by 商语童 on 2020/9/23.
+//  Created by EmilyShang on 2020/9/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Text("SmartGas")
+            .font(.largeTitle)
+            .multilineTextAlignment(.leading)
+            .padding().position(x:240,y:400)
+
+        Image("Group 1").position(x: 200, y: 200)
+
     }
+    
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDisplayName("Launch Page")
+            .preferredColorScheme(.dark)
     }
 }
