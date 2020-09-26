e1a90f5f4f3479893cf1e48c5be1afb28540ed6d
yshang616 <yshang@macalester.edu>
2020-09-27 05:10:50 +0800

    Initial Commit

---
+++ SmartGas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartGas
+//
+//  Created by 商语童 on 2020/9/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
