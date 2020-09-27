ee68ec446a31d12922dd27be1caa3215a1555e86
yshang616 <yshang@macalester.edu>
2020-09-28 01:35:54 +0800

    Added List and Navigation Pages

---
+++ SmartGas/SearchBar.swift
@@ -0,0 +1,9 @@
+//
+//  SearchBar.swift
+//  SmartGas
+//
+//  Created by 商语童 on 2020/9/27.
+//
+
+
+import Foundation
--- SmartGas/ViewController.swift
+++ SmartGas/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet var tableView: UITableView!
+    
+    let myData = ["gas1", "gas2", "gas3", "gas4", "gas5"]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//  Ignore these for now please!
+//        tableView.delegate=self
+//        tableView.dataSource=self
     }
-
+//  Ignore these for now please!
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return myData.count
+//    }
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",
+//                                                 for: indexPath)
+//        cell.textLabel?.text=myData[indexPath.row]
+//        return cell
+//    }
 
 }
 
